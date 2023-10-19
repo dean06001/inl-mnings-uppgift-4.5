12c2ecae38ed9e34101ac4ed85eb4ef979751181
dean06001 <dean06001@E-PW0234KC>
2023-10-19 15:13:01 +0200

    Add project files.

---
+++ inlämnings uppgift 4.5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Uppgift_4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in heltal");
+            string heltal = Console.ReadLine();
+            for (int i = 0; i < heltal.Length; i++)
+            {
+                string tecken = heltal[i].ToString();
+                int siffra = int.Parse(tecken);
+                Console.Write($"{siffra + 1}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
